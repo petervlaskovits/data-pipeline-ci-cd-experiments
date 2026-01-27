a18b2edb3340afc6a986b464eda678440d056580
Peter Vlaskovits <petervlaskovits@gmail.com>
2026-01-27 08:08:30 -0800

    Change docs

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 def clean_column(column: list[str]) -> tuple[list[float], list[str], list[str]]:
     """
-    Cleans a column consisting of the hourly salary, and extracts currency, wage, and the salary information into a tuple.
+    Cleans a column consisting of the hourly salary, and extracts the original column, currency, wage, and the salary information into a tuple.
     """
     currency = ["USD" if row[0] == "$" else None for row in column]
     wage = [float(row.strip("$/hr").strip("per hour")) for row in column]
